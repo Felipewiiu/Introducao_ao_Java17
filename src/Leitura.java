ce3c557729a48af5ea343f36028d2dc43e98e68b
Felipe Lima <felipe.lima@FLEX.LOCAL>
2023-09-29 17:33:04 -0300

    Trabalhando com repetições

--- src/Leitura.java
+++ src/Leitura.java
@@ -6,17 +6,14 @@ public class Leitura {
 
         System.out.println("Digite seu filme favorito");
         String filme = leitura.nextLine();
-
-
         System.out.println("Qual é o ano de lançamento? ");
         int anoDeLancamento = leitura.nextInt();
-
-
         System.out.println("Diga a sua avaliação para o filme ");
         double avaliacao = leitura.nextDouble();
 
         System.out.println(filme);
         System.out.println(anoDeLancamento);
         System.out.println(avaliacao);
+
     }
 }
